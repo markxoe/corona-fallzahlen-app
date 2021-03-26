2794e98923dc3ca4b365b459f28714ccd1cd7bee
markxoe <mark@oude-elberink.de>
2021-03-26 14:09:06 +0100

    :lipstick: Grid Layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -93,153 +93,177 @@ const PageHome: React.FC = () => {
           <IonRefresherContent />
         </IonRefresher>
 
-        <IonCard>
-          <IonCardContent>
-            {state.temp.cache.data?.germany
-              ? "Daten vom " +
-                new Date(
-                  state.temp.cache.data.germany.meta.lastUpdate
-                ).toLocaleDateString()
-              : "Daten nicht geladen"}
-          </IonCardContent>
-        </IonCard>
+        <IonGrid>
+          <IonRow>
+            <IonCol size="12">
+              <IonCard>
+                <IonCardContent>
+                  {state.temp.cache.data?.germany
+                    ? "Daten vom " +
+                      new Date(
+                        state.temp.cache.data.germany.meta.lastUpdate
+                      ).toLocaleDateString()
+                    : "Daten nicht geladen"}
+                </IonCardContent>
+              </IonCard>
+            </IonCol>
 
-        {favorites.map((i) => (
-          <StateOrDistrictCard
-            stateordistrict={i}
-            isFavorite={true}
-            toggleFavorite={() => dispatch(ActionRemoveFavorite(i.id ?? ""))}
-          />
-        ))}
+            {favorites.map((i) => (
+              <IonCol size="auto" sizeLg="4" sizeSm="6" sizeXs="12">
+                <StateOrDistrictCard
+                  stateordistrict={i}
+                  isFavorite={true}
+                  toggleFavorite={() =>
+                    dispatch(ActionRemoveFavorite(i.id ?? ""))
+                  }
+                />
+              </IonCol>
+            ))}
+            <IonCol size="auto" sizeLg="4" sizeSm="6" sizeXs="12">
+              <IonCard>
+                <IonCardHeader>
+                  <IonCardTitle>Deutschland</IonCardTitle>
+                </IonCardHeader>
+                <IonCardContent>
+                  <IonGrid>
+                    <IonRow>
+                      <IonCol>Fälle</IonCol>
+                      <IonCol>
+                        {showOrSkeleton(
+                          state.temp.cache.data?.germany.cases,
+                          displayValue
+                        )}
+                      </IonCol>
+                    </IonRow>
+                    <IonRow>
+                      <IonCol>Inzidenz</IonCol>
+                      <IonCol>
+                        {showOrSkeleton(
+                          state.temp.cache.data?.germany.weekIncidence,
+                          displayValue
+                        )}
+                      </IonCol>
+                    </IonRow>
+                    <IonRow>
+                      <IonCol>Neuinfektionen</IonCol>
+                      <IonCol>
+                        {showOrSkeleton(
+                          state.temp.cache.data?.germany.weekIncidence,
+                          displayValue
+                        )}
+                      </IonCol>
+                    </IonRow>
+                    <IonRow>
+                      <IonCol>Todesfälle</IonCol>
+                      <IonCol>
+                        {showOrSkeleton(
+                          state.temp.cache.data?.germany.delta.deaths,
+                          displayValue
+                        )}
+                      </IonCol>
+                    </IonRow>
+                    <IonRow>
+                      <IonCol>R Wert</IonCol>
+                      <IonCol>
+                        {showOrSkeleton(
+                          state.temp.cache.data?.germany.r.value,
+                          displayValue
+                        )}
+                      </IonCol>
+                    </IonRow>
+                  </IonGrid>
+                </IonCardContent>
+              </IonCard>
+            </IonCol>
+            <IonCol size="auto" sizeLg="4" sizeSm="6" sizeXs="12">
+              <IonCard routerLink="/districts">
+                <IonCardHeader>
+                  <IonCardTitle>Landkreise</IonCardTitle>
+                </IonCardHeader>
+                <img
+                  src="https://api.corona-zahlen.org/map/districts"
+                  className="lil-img-padding"
+                  alt="Corona Landkarte Landkreise"
+                />
+              </IonCard>
+            </IonCol>
+            <IonCol size="auto" sizeLg="4" sizeSm="6" sizeXs="12">
+              <IonCard routerLink="/states">
+                <IonCardHeader>
+                  <IonCardTitle>Bundesländer</IonCardTitle>
+                </IonCardHeader>
+                <img
+                  src="https://api.corona-zahlen.org/map/states"
+                  className="lil-img-padding"
+                  alt="Corona Landkarte Bundesländer"
+                />
+              </IonCard>
+            </IonCol>
 
-        <IonCard>
-          <IonCardHeader>
-            <IonCardTitle>Deutschland</IonCardTitle>
-          </IonCardHeader>
-          <IonCardContent>
-            <IonGrid>
-              <IonRow>
-                <IonCol>Fälle</IonCol>
-                <IonCol>
-                  {showOrSkeleton(
-                    state.temp.cache.data?.germany.cases,
-                    displayValue
-                  )}
-                </IonCol>
-              </IonRow>
-              <IonRow>
-                <IonCol>Inzidenz</IonCol>
-                <IonCol>
-                  {showOrSkeleton(
-                    state.temp.cache.data?.germany.weekIncidence,
-                    displayValue
-                  )}
-                </IonCol>
-              </IonRow>
-              <IonRow>
-                <IonCol>Neuinfektionen</IonCol>
-                <IonCol>
-                  {showOrSkeleton(
-                    state.temp.cache.data?.germany.weekIncidence,
-                    displayValue
-                  )}
-                </IonCol>
-              </IonRow>
-              <IonRow>
-                <IonCol>Todesfälle</IonCol>
-                <IonCol>
-                  {showOrSkeleton(
-                    state.temp.cache.data?.germany.delta.deaths,
-                    displayValue
-                  )}
-                </IonCol>
-              </IonRow>
-              <IonRow>
-                <IonCol>R Wert</IonCol>
-                <IonCol>
-                  {showOrSkeleton(
-                    state.temp.cache.data?.germany.r.value,
-                    displayValue
-                  )}
-                </IonCol>
-              </IonRow>
-            </IonGrid>
-          </IonCardContent>
-        </IonCard>
-
-        <IonCard routerLink="/districts">
-          <IonCardHeader>
-            <IonCardTitle>Landkreise</IonCardTitle>
-          </IonCardHeader>
-          <img
-            src="https://api.corona-zahlen.org/map/districts"
-            className="lil-img-padding"
-            alt="Corona Landkarte Landkreise"
-          />
-        </IonCard>
-        <IonCard routerLink="/states">
-          <IonCardHeader>
-            <IonCardTitle>Bundesländer</IonCardTitle>
-          </IonCardHeader>
-          <img
-            src="https://api.corona-zahlen.org/map/states"
-            className="lil-img-padding"
-            alt="Corona Landkarte Bundesländer"
-          />
-        </IonCard>
-
-        <IonCard>
-          <IonCardHeader>
-            <IonCardTitle>Informationen</IonCardTitle>
-          </IonCardHeader>
-          <IonCardContent>
-            <IonGrid>
-              <IonRow>
-                <IonCol>Kontakt</IonCol>
-                <IonCol className="ion-text-end">
-                  {showOrSkeleton(state.temp.cache.data?.germany.meta.contact)}
-                </IonCol>
-              </IonRow>
-              <IonRow>
-                <IonCol>Quelle</IonCol>
-                <IonCol className="ion-text-end">
-                  {showOrSkeleton(state.temp.cache.data?.germany.meta.source)}
-                </IonCol>
-              </IonRow>
-              <IonRow>
-                <IonCol>Source Code zur API</IonCol>
-                <IonCol className="ion-text-end">
-                  <a href={state.temp.cache.data?.germany.meta.info}>
-                    {showOrSkeleton(state.temp.cache.data?.germany.meta.info)}
-                  </a>
-                </IonCol>
-              </IonRow>
-              <IonRow>
-                <IonCol>
-                  Achtung: Die angezeigten Daten können von den tatsächlichen
-                  Daten abweichen. Diese App sollte nicht für medizinische
-                  Entscheidungen verwendet werden
-                </IonCol>
-              </IonRow>
-            </IonGrid>
-          </IonCardContent>
-        </IonCard>
-        <IonCard>
-          <IonCardHeader>
-            <IonCardTitle>Über</IonCardTitle>
-            <IonCardSubtitle>Über die App</IonCardSubtitle>
-          </IonCardHeader>
-          <IonCardContent>
-            <p>
-              Fehler gefunden? Verbesserungsvorschläge? Diese App ist
-              OpenSource!{" "}
-              <a href="https://github.com/markxoe/corona-fallzahlen-app">
-                Zum Projekt
-              </a>
-            </p>
-            <p>App Version: {packageJSON.version}</p>
-          </IonCardContent>
-        </IonCard>
+            <IonCol size="auto" sizeSm="6" sizeXs="12">
+              <IonCard>
+                <IonCardHeader>
+                  <IonCardTitle>Informationen</IonCardTitle>
+                </IonCardHeader>
+                <IonCardContent>
+                  <IonGrid>
+                    <IonRow>
+                      <IonCol>Kontakt</IonCol>
+                      <IonCol className="ion-text-end">
+                        {showOrSkeleton(
+                          state.temp.cache.data?.germany.meta.contact
+                        )}
+                      </IonCol>
+                    </IonRow>
+                    <IonRow>
+                      <IonCol>Quelle</IonCol>
+                      <IonCol className="ion-text-end">
+                        {showOrSkeleton(
+                          state.temp.cache.data?.germany.meta.source
+                        )}
+                      </IonCol>
+                    </IonRow>
+                    <IonRow>
+                      <IonCol>Source Code zur API</IonCol>
+                      <IonCol className="ion-text-end">
+                        <a href={state.temp.cache.data?.germany.meta.info}>
+                          {showOrSkeleton(
+                            state.temp.cache.data?.germany.meta.info
+                          )}
+                        </a>
+                      </IonCol>
+                    </IonRow>
+                    <IonRow>
+                      <IonCol>
+                        Achtung: Die angezeigten Daten können von den
+                        tatsächlichen Daten abweichen. Diese App sollte nicht
+                        für medizinische Entscheidungen verwendet werden
+                      </IonCol>
+                    </IonRow>
+                  </IonGrid>
+                </IonCardContent>
+              </IonCard>
+            </IonCol>
+            <IonCol size="12">
+              <IonCard>
+                <IonCardHeader>
+                  <IonCardTitle>Über</IonCardTitle>
+                  <IonCardSubtitle>Über die App</IonCardSubtitle>
+                </IonCardHeader>
+                <IonCardContent>
+                  <p>
+                    Fehler gefunden? Verbesserungsvorschläge? Diese App ist
+                    OpenSource!{" "}
+                    <a href="https://github.com/markxoe/corona-fallzahlen-app">
+                      Zum Projekt
+                    </a>
+                  </p>
+                  <p>App Version: {packageJSON.version}</p>
+                </IonCardContent>
+              </IonCard>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
       </IonContent>
       <IonFooter>
         <IonProgressBar hidden={!state.temp.loading} type="indeterminate" />
